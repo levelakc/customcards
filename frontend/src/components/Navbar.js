da88991feb4660ed2c3e5da4b16292f6081208f3
Maximilian Gendel <max@Maximilians-MacBook-Air.local>
2025-11-16 19:23:12 +0200

    sdsss

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -49,7 +49,7 @@ export default function Navbar() {
     }
 
     return (
-        <nav className="bg-black bg-opacity-50 backdrop-blur-lg sticky top-0 z-50 text-white shadow-lg">
+        <nav className="bg-black bg-opacity-50 backdrop-blur-lg fixed top-0 z-50 w-full text-white shadow-lg">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-20">
                     
